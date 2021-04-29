4db7148b7abdfe0085af57152033e250718ed8f6
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-04-29 19:44:41 +0530

    Create TestRangeCalculator.cpp

---
+++ TestRangeCalculator.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "typewise-alert.h"
+
+//Test Driven Ranges
+
+TEST_CASE("Case for detecting number of continuous range in the reading array given") {
+  int ChargingCurrentReadings[] = {3, 3, 5, 4, 10, 11, 12};
+  int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
+  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 2);
